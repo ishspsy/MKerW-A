d372ca2f244ccb00c71ce7781bf97bcbfad2b080
ishspsy <ishspsy@gmail.com>
2018-02-10 16:53:34 -0500

    Update Generating_Heatmap.m

--- Generating_Figures/Generating_Heatmap.m
+++ Generating_Figures/Generating_Heatmap.m
@@ -19,7 +19,7 @@ h.ColorLimits=[0 0.5];
 load('minheat.csv')
 yvalue={'KIRP','SARC','PRAD','BRCA','KIRC','MESO','THCA','CESC','UVM','READ','BLCA','COAD','LIHC','UCEC','LUSC','ESCA','STAD','OV','PAAD','HNSC','LUAD','LGG'};
 xvalue={'MKerW-A','MKer-A','K-R','Cons-A','Cons-R','iCluster-A','S-R','SIM-R','P-A','C-A','SIM-A','K-M','S-A','Cons-M','Ker-A','SIM-M','K-A','K-C','Cons-C','SIM-C','S-M','S-C'};
-h=heatmap(xvalue,yvalue,minheat1);
+h=heatmap(xvalue,yvalue,minheat);
 h.Colormap=flipud(hot);
 h.GridVisible='off';
 h.Title='Area_Min';
@@ -30,7 +30,7 @@ h.ColorLimits=[0.3 1];
 load('diff.csv')
 yvalue={'KIRP','SARC','PRAD','BRCA','KIRC','MESO','THCA','CESC','UVM','READ','BLCA','COAD','LIHC','UCEC','LUSC','ESCA','STAD','OV','PAAD','HNSC','LUAD','LGG'};
 xvalue={'MKer-A','K-R','Cons-A','Cons-R','iCluster-A','S-R','SIM-R','P-A','C-A','SIM-A','K-M','S-A','Cons-M','Ker-A','SIM-M','K-A','K-C','Cons-C','SIM-C','S-M','S-C'};
-h=heatmap(xvalue,yvalue,diff1');
+h=heatmap(xvalue,yvalue,diff');
 h.Colormap=flipud(hot);
 h.GridVisible='off';
 h.Title='L1-dif';
