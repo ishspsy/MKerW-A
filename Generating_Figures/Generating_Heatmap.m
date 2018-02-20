a98108189e82b199e619c57471705c50af68b57a
ishspsy <ishspsy@gmail.com>
2018-02-19 22:06:26 -0500

    Update Generating_Heatmap.m

--- Generating_Figures/Generating_Heatmap.m
+++ Generating_Figures/Generating_Heatmap.m
@@ -1,7 +1,7 @@
 addpath(genpath(pwd))
 
 
-%% Figure2(A-C)
+%% Figures 4-5
 
 %pvalue
 load('pheat.csv')
@@ -25,18 +25,6 @@ h.GridVisible='off';
 h.Title='Area_Min';
 h.FontSize = 12;
 h.ColorLimits=[0.3 1];
-
-%diff
-load('diff.csv')
-yvalue={'KIRP','SARC','PRAD','BRCA','KIRC','MESO','THCA','CESC','UVM','READ','BLCA','COAD','LIHC','UCEC','LUSC','ESCA','STAD','OV','PAAD','HNSC','LUAD','LGG'};
-xvalue={'MKer-A','K-R','Cons-A','Cons-R','iCluster-A','S-R','SIM-R','P-A','C-A','SIM-A','K-M','S-A','Cons-M','Ker-A','SIM-M','K-A','K-C','Cons-C','SIM-C','S-M','S-C'};
-h=heatmap(xvalue,yvalue,diff');
-h.Colormap=flipud(hot);
-h.GridVisible='off';
-h.Title='L1-dif';
-h.FontSize = 12;
-h.ColorLimits=[-2.9 15];
-
 
 
 
